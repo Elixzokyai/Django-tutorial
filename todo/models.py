9b31e584c6235ebf86e8e03e57a383a3af305962
Elixzokyai <elijahmuendo100@gmail.com>
2024-11-13 16:51:35 +0300

    added the Python elements

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,8 @@
+from enum import unique
+
 from django.db import models
+from django.utils.formats import date_format
+
 
 # Create your models here.
 # this step should create a table task in our db where the attributes will be the column
@@ -9,3 +13,14 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    Author = models.CharField(max_length=100)
+    published = models.DateField()
+
+
+    def __str__(self):
+        return self.title
+
