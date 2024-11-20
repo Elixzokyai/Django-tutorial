b1dc3ae67565aebeae1367f5f7736ab7bc0b3163
Elixzokyai <elijahmuendo100@gmail.com>
2024-11-20 13:36:20 +0300

    Added the second elements

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,9 @@
 ## a method to list our tasks
 ## a method to create the tasks
 # method to list task
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+
 from .models import Task
 from .forms import TaskForm
 def task_list(request):
@@ -22,5 +24,21 @@ def task_create(request):
         form = TaskForm()
     return render(request, 'todo/task_form.html',{'form': form})
 
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()  # Corrected line
+        return redirect('task_list')
+    return render(request, 'todo/task_confirm_delete.html', {'task': task})
 
 
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)  # Corrected line
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/task_form.html', {'form': form})
